3e6891a4eb0b73f5ddc28898d3511fa2c007beef
suxiongwei <suxiongwei@xywy.com>
2018-08-20 18:16:48 +0800

    测试下idea的git版本控制

--- src/main/java/com/xywy/lucene/SearchApp.java
+++ src/main/java/com/xywy/lucene/SearchApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SearchApp {
+    //项目启动类
     public static void main(String[] args) {
         SpringApplication.run(SearchApp.class,args);
     }
